d085296af7f21a6be8942db4de0fb9c4684fc2c9
syggit <71319002+syggit@users.noreply.github.com>
2024-06-07 12:47:07 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask
 app = Flask(__name__)
+cnt=0
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    global cnt
+    cnt += 1
+    return 'Hello World! %d' % cnt
